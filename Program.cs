51fd62c48151cbc5dac64fe5c4343cd6cd646ed5
ShaleevDmitriy <dmitriy.shaleev222@gmail.com>
2025-12-23 09:49:07 +0300

    123

--- Program.cs
+++ Program.cs
@@ -5,3 +5,7 @@ using System.Linq;
 // Входные данные
 List<int> numbers = new List<int> { 3, 10, 1, 5, 8 };
 Console.WriteLine("Исходный список: " + string.Join(", ", numbers));
+// Сортировка по убыванию через LINQ
+var sortedDesc = numbers.OrderByDescending(n => n).ToList();
+
+Console.WriteLine("Отсортировано по убыванию: " + string.Join(", ", sortedDesc));
