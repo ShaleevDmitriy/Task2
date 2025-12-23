2dde85daa96cc716db2a6f9854f5816d6c603f5c
ShaleevDmitriy <dmitriy.shaleev222@gmail.com>
2025-12-23 09:33:19 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// Входные данные
+List<int> numbers = new List<int> { 3, 10, 1, 5, 8 };
+Console.WriteLine("Исходный список: " + string.Join(", ", numbers));
